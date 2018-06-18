30ca8bc0349bce8c4584576a66f6f1b4fb199a61
joshuous <joshuous@gmail.com>
2018-06-18 18:37:01 +0200

    sched/tune: Reset Dynamic Schedtune Boost only if no more boosts running
    We will need to take care to ensure that every do_stune_boost() we call
    is followed eventually by a reset_stune_boost() so that
    stune_boost_count is managed correctly.
    
    This allows us to stack several Dynamic Schedtune Boosts and reset only
    when all Dynamic Schedtune Boosts have been disengaged.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -37,6 +37,7 @@ struct boost_slot {
 static DEFINE_MUTEX(stune_boost_mutex);
 static struct schedtune *getSchedtune(char *st_name);
 static int dynamic_boost(struct schedtune *st, int boost);
+static int stune_boost_count = 0;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 /* Performance Boost region (B) threshold params */
@@ -915,6 +916,7 @@ static int _do_stune_boost(struct schedtune *st, int boost)
 	int ret = 0;
 
 	mutex_lock(&stune_boost_mutex);
+	++stune_boost_count;
 
 	/* Boost if new value is greater than current */
 	if (boost > st->boost)
@@ -934,7 +936,11 @@ int reset_stune_boost(char *st_name)
 		return -EINVAL;
 
 	mutex_lock(&stune_boost_mutex);
-	ret = dynamic_boost(st, st->boost_default);
+	if (stune_boost_count == 1)
+		ret = dynamic_boost(st, st->boost_default);
+
+	if (stune_boost_count >= 1)
+		--stune_boost_count;
 	mutex_unlock(&stune_boost_mutex);
 
 	return ret;
